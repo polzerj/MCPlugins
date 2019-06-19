c93985ba0f17b571ae722c9a1f045c6c2e4fca93
jonny <jojo.polzer@hotmail.com>
2019-06-19 14:39:29 +0200

    implemented no ignit mode

--- AntiTNTPlugin/src/antitnt/Main.java
+++ AntiTNTPlugin/src/antitnt/Main.java
@@ -1,72 +1,67 @@
 package antitnt;
 
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import net.md_5.bungee.api.ChatColor;
 
-public class Main extends JavaPlugin{
+public class Main extends JavaPlugin {
 
 	static boolean pluginActivated = true;
 	static boolean canIgnite = true;
-	
+
+	static Main main;
+
 	@Override
-	public void onEnable()
-	{
-		//Setup Manager
+	public void onEnable() {
+		// Setup Manager
 		PluginManager pManager = getServer().getPluginManager();
-		//Create Listener
+		// Create Listener
 		pManager.registerEvents(new TNTListener(), this);
+		main = this;
 	}
+
 	
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args)
-	{
-		if (command.getName().equalsIgnoreCase("TNT"))
-		{
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		if (command.getName().equalsIgnoreCase("TNT")) {
 			boolean done = false;
-			
-			if (args.length == 1)
-			{
-				if (args[0].equalsIgnoreCase("false"))
-				{
+
+			if (args.length == 1) {
+				if (args[0].equalsIgnoreCase("false")) {
 					pluginActivated = true;
 					done = true;
 					sender.sendMessage("TNT Plugin is now enabled");
-				}
-				else if (args[0].equalsIgnoreCase("true"))
-				{
+				} else if (args[0].equalsIgnoreCase("true")) {
 					pluginActivated = false;
 					done = true;
 					sender.sendMessage("TNT Plugin is now disabled");
 				}
 			}
-			
-			if (args.length==2) {
+
+			if (args.length == 2) {
 				if (args[0].equalsIgnoreCase("ignite")) {
 					if (args[1].equalsIgnoreCase("false")) {
-						canIgnite=false;
-						done=true;
+						canIgnite = false;
+						done = true;
 						sender.sendMessage("TNT is not flammable");
-					}
-					else if(args[1].equalsIgnoreCase("true")) {
-						canIgnite=true;
-						done=true;
+					} else if (args[1].equalsIgnoreCase("true")) {
+						canIgnite = true;
+						done = true;
 						sender.sendMessage("TNT is now flammable");
-				}
+					}
 				}
 			}
-			
-			if (!done)
-			{
-				sender.sendMessage("§c§lEingabe ungültig /TNT [true,false] §r");
+
+			if (!done) {
+				sender.sendMessage(ChatColor.BOLD + "" + ChatColor.RED + "Eingabe ung�ltig /TNT [true,false]");
 			}
-			
+
 			return true;
 		}
-		
+
 		return false;
 	}
-	}
-	
-	
+}
--- AntiTNTPlugin/src/antitnt/TNTListener.java
+++ AntiTNTPlugin/src/antitnt/TNTListener.java
@@ -1,39 +1,63 @@
 package antitnt;
 
+import com.destroystokyo.paper.event.entity.EntityAddToWorldEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.block.BlockIgniteEvent;
 import org.bukkit.event.block.BlockRedstoneEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.event.entity.EntitySpawnEvent;
+import org.bukkit.event.entity.ExplosionPrimeEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
+
 public class TNTListener implements Listener {
-	@EventHandler
+	/*@EventHandler
 	public void onPlayerInteract(PlayerInteractEvent e) {
-		if(e.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
-			if(e.getClickedBlock().getType().equals(Material.TNT) && e.getItem().getType().equals(Material.FLINT_AND_STEEL)) {
-				e.setCancelled(true);
+		if (!Main.canIgnite&&Main.pluginActivated) {
+			if (e.getAction().equals(Action.RIGHT_CLICK_BLOCK)) {
+				if (e.getClickedBlock().getType().equals(Material.TNT)
+						&& e.getItem().getType().equals(Material.FLINT_AND_STEEL)) {
+					e.setCancelled(true);
+				}
 			}
 		}
 	}
-	
+
 	@EventHandler
-	public void onBBlockRedstoneEvent(BlockRedstoneEvent e) {
-		System.out.print("Redstone");
-		if(e.getBlock().getType().equals(Material.TNT)) {
-			System.out.print("TNT");
-			e.setNewCurrent(0);
+	public void onExplosionPrime(ExplosionPrimeEvent e){
+		if (e.getEntityType().equals(EntityType.PRIMED_TNT) && Main.pluginActivated) {
+			e.setCancelled(true);
+		}
+	}*/
+
+	@EventHandler
+	public void onEntityAddToWorld(EntityAddToWorldEvent e){
+		if(e.getEntityType().equals(EntityType.PRIMED_TNT)&&Main.pluginActivated&&!Main.canIgnite){
+			//System.out.println("TNT was added to world");
+			Location loc = e.getEntity().getLocation();
+
+			e.getEntity().remove();
+			Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(Main.main, new Runnable() {
+				@Override
+				public void run() {
+					loc.getWorld().getBlockAt(loc).setType(Material.TNT,false);
+				}
+			}, 1L);
+
 		}
 	}
-	
+
 	@EventHandler
 	public void onEntityExplodeEvent(EntityExplodeEvent e) {
-		if (e.getEntityType().equals(EntityType.PRIMED_TNT) && Main.pluginActivated){
+		if (e.getEntityType().equals(EntityType.PRIMED_TNT) && Main.pluginActivated) {
 			e.setCancelled(true);
 		}
 	}
-	
-	
+
 }
