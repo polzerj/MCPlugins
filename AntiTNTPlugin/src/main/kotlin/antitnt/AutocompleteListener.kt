f9e521c8f61d1d796aa90ffd26d6b73888875486
polzerj <jojo.polzer@hotmail.com>
2020-08-26 10:51:24 +0200

    fix autocomplete

--- AntiTNTPlugin/src/main/kotlin/antitnt/AutocompleteListener.kt
+++ AntiTNTPlugin/src/main/kotlin/antitnt/AutocompleteListener.kt
@@ -14,9 +14,10 @@ class AutocompleteListener : TabCompleter {
         val completions = mutableListOf<String>()
         //copy matches of first argument from list (ex: if first arg is 'm' will return just 'minecraft'
         var prop = FIRST_LVL_COMMANDS.toMutableList()
-        if (args[0].equals("ignite", ignoreCase = true)) {
+        if (args[0].equals("ignite", ignoreCase = true)  ) {
+            if (args.size>2)return mutableListOf<String>()
             prop = mutableListOf("true","false")
-        }
+        }else if (args.size>1) return mutableListOf<String>()
         StringUtil.copyPartialMatches(args.last(), prop, completions)
         //sort the list
         completions.sort()
